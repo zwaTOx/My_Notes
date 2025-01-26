53fae5fa509eff4d2887da0bf4049611838a25a6
zwaTOx <yakov.g.ruslanovich@gmail.com>
2025-01-26 21:32:19 +0700

    Сreatу a database in a class. add deleting elements

--- DB_controller.py
+++ DB_controller.py
@@ -2,40 +2,65 @@ import sqlite3
 import os
 import datetime
 
-# Определяем путь к файлу базы данных
-base_dir = os.path.dirname(os.path.abspath(__file__))
-db_path = os.path.join(base_dir, 'data', 'my_notes.db')
 
-# Устанавливаем соединение с базой данных
-connection = sqlite3.connect(db_path)
-cursor = connection.cursor()
+class NotesDatabase:
+    def __init__(self, name = 'Notes', filename = 'my_notes.db'):
+        self.name = name
+        base_dir = os.path.dirname(os.path.abspath(__file__))
+        self.path = os.path.join(base_dir, 'data', filename)
+        self.connection = None
+        self.cursor = None
 
-# Создаем таблицу
-cursor.execute('''
-CREATE TABLE IF NOT EXISTS Notes (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    title TEXT NOT NULL,
-    description TEXT,
-    date TEXT NOT NULL,
-    time TEXT NOT NULL
-)
-''')
+        self.connect()
+        self.create_table()
+    
+    def connect(self):
+        #Подключение к базе данных.
+        if self.connection is None:
+            self.connection = sqlite3.connect(self.path)
+            self.cursor = self.connection.cursor()
 
-#Шаблон для вставки элемента в таблицу
-adding_note = 'INSERT INTO Notes (title, description, date, time) VALUES (?, ?, ?, ?);'
+    def create_table(self):
+        #Создание таблицы, если она не существует.
+        self.cursor.execute(f'''
+        CREATE TABLE IF NOT EXISTS "{self.name}" (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            title TEXT NOT NULL,
+            description TEXT,
+            date TEXT NOT NULL,
+            time TEXT NOT NULL
+        )
+        ''')
 
-#Тестовое наполнение базы данных
-cursor.execute(adding_note, ('My Note', 'This is a description', datetime.datetime.now().date().isoformat(), datetime.datetime.now().time().isoformat()))
+    def add_note(self, title, description=''):
+        #Добавление элемента в базу данных.
+        adding_note = f'INSERT INTO {self.name} (title, description, date, time) VALUES (?, ?, ?, ?);'
+        self.cursor.execute(adding_note, (title, description, datetime.datetime.now().date().isoformat(), datetime.datetime.now().time().isoformat()))
 
-#Вывод в обратном порядке (Для плашек на главном экране)
-cursor.execute('SELECT * FROM Notes ORDER BY ID DESC')
-rows = cursor.fetchall()
-for row in rows:
-    print(row)
+    def delete_notes(self, *args: int):
+        for id in args:
+            self.cursor.execute(f'DELETE FROM {self.name} WHERE id = {id}')
 
-# Сохраняем изменения и закрываем соединение
-connection.commit()
-connection.close()
+    def display_notes(self):
+        #Вывод заметок в обратном порядке.
+        self.cursor.execute(f'SELECT * FROM {self.name} ORDER BY id DESC')
+        rows = self.cursor.fetchall()
+        for row in rows:
+            print(row)
 
-# Выводим абсолютный путь для файла my_notes.db
-print(db_path)
+    def close(self):
+        #Закрытие соединения с базой данных.
+        self.connection.commit()
+        self.connection.close()
+    
+    def drop_database(self):
+        #Удаление базы данных
+        os.remove(self.path)
+    
+db = NotesDatabase()
+db.add_note('t2', 'heeey')
+db.add_note('t1')
+db.delete_notes(1,2)
+db.display_notes()
+db.close()
+db.drop_database()
